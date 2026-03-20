ee0394b3c578788817592d6d974aa480da34b70a
Chetan7171 <128369798+Chetan7171@users.noreply.github.com>
2026-03-21 03:49:33 +0530

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
