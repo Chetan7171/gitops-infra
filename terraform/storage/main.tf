965293d7ba827b0058e095fb6de942c61db7890d
Chetan7171 <128369798+Chetan7171@users.noreply.github.com>
2026-03-21 04:01:44 +0530

    Update main.tf

--- terraform/storage/main.tf
+++ terraform/storage/main.tf
@@ -1,3 +1,17 @@
+#######################################
+# Namespace for PostgreSQL
+#######################################
+
+resource "kubernetes_namespace" "postgres" {
+  metadata {
+    name = "postgres"
+  }
+}
+
+#######################################
+# StorageClass for EBS gp3
+#######################################
+
 resource "kubernetes_storage_class" "gp3" {
   metadata {
     name = "gp3"
@@ -11,4 +25,61 @@ resource "kubernetes_storage_class" "gp3" {
 
   reclaim_policy = "Retain"
   volume_binding_mode = "WaitForFirstConsumer"
+}
+
+#######################################
+# Secret for PostgreSQL password
+#######################################
+
+resource "kubernetes_secret" "postgres_secret" {
+  metadata {
+    name      = "postgres-secret"
+    namespace = kubernetes_namespace.postgres.metadata[0].name
+  }
+
+  data = {
+    POSTGRES_PASSWORD = base64encode("postgres")
+  }
+
+  type = "Opaque"
+}
+
+#######################################
+# PostgreSQL via Helm
+#######################################
+
+resource "helm_release" "postgresql" {
+  name       = "postgresql"
+  namespace  = kubernetes_namespace.postgres.metadata[0].name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
+
+  set {
+    name  = "auth.username"
+    value = "postgres"
+  }
+
+  set {
+    name  = "auth.password"
+    value = "postgres"
+  }
+
+  set {
+    name  = "auth.database"
+    value = "users"
+  }
+
+  set {
+    name  = "primary.persistence.storageClass"
+    value = "gp3"
+  }
+
+  set {
+    name  = "primary.persistence.size"
+    value = "10Gi"
+  }
+}
+
+output "postgresql_service" {
+  value = "postgresql.postgres.svc.cluster.local"
 }
