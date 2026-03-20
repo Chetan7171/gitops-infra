bf7e0eaa633bcd48c36012f9d4c2c8b3a3e1a032
Chetan7171 <128369798+Chetan7171@users.noreply.github.com>
2026-03-21 03:59:20 +0530

    Update main.tf

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,3 +1,7 @@
+###############################
+# IAM Role for ALB Controller
+###############################
+
 data "aws_iam_policy_document" "alb_assume_role_policy" {
   statement {
     effect = "Allow"
@@ -23,11 +27,62 @@ resource "aws_iam_role" "alb_role" {
 }
 
 resource "aws_iam_policy" "alb_policy" {
-  name        = "AWSLoadBalancerControllerIAMPolicy"
-  policy      = file("${path.module}/alb_iam_policy.json")
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  policy = file("${path.module}/alb_iam_policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "alb_attach" {
   policy_arn = aws_iam_policy.alb_policy.arn
   role       = aws_iam_role.alb_role.name
+}
+
+###############################
+# Kubernetes ServiceAccount
+###############################
+
+resource "kubernetes_service_account" "alb_service_account" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_role.arn
+    }
+  }
+}
+
+###############################
+# Helm Install AWS Load Balancer Controller
+###############################
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.alb_service_account.metadata[0].name
+  }
+
+  set {
+    name  = "region"
+    value = "ap-south-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
 }
